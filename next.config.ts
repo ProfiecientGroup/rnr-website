18c58988692bfd14b835b2bc7b15479aa6d020c2
ProfiecientGroup <binarybrains34@gmail.com>
2025-03-01 01:12:31 +0530

    fix issue

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: "export", // Enable static export
+  // output: "export", // Enable static export
   reactStrictMode: true,
   trailingSlash: true, // Ensures correct routing in S3
 };
